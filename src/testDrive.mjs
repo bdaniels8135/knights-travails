c41985ed5b08d61d00a6d21fa5aedf0cd0235f89
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-06 20:51:21 -0500

    Create test drive script to try things out

---
+++ src/testDrive.mjs
@@ -0,0 +1,7 @@
+import knightMoves from "./knightMoves.mjs";
+
+console.log(knightMoves([0, 0], [1, 2]));
+console.log(knightMoves([0, 0], [3, 3]));
+console.log(knightMoves([3, 3], [0, 0]));
+console.log(knightMoves([0, 0], [7, 7]));
+console.log(knightMoves([4, 4], [3, 3]));
