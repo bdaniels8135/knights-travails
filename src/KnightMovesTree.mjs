1e4fbd62801bc4a2b3a63f8fc34e39e46c2b8b19
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-12-06 20:29:00 -0500

    Implement path search function

--- src/KnightMovesTree.mjs
+++ src/KnightMovesTree.mjs
@@ -3,8 +3,6 @@ import KNode from "./KNode.mjs";
 export default class KnightMovesTree {
   #root;
 
-  #endPos;
-
   constructor([startX, startY]) {
     const startPos = startX + 8 * startY;
     this.#root = KnightMovesTree.#buildTree(startPos);
@@ -23,14 +21,14 @@ export default class KnightMovesTree {
       [1, -2],
       [1, 2],
     ];
-    const possibleMoveCoords = possibleMoveVectors.map((xDiff, yDiff) => [
+    const possibleMoveCoords = possibleMoveVectors.map(([xDiff, yDiff]) => [
       xCoord + xDiff,
       yCoord + yDiff,
     ]);
     const legalMoveCoords = possibleMoveCoords.filter(
       ([x, y]) => x >= 0 && x <= 7 && y >= 0 && y <= 7
     );
-    const legalMovePositions = legalMoveCoords.map((x, y) => x + 8 * y);
+    const legalMovePositions = legalMoveCoords.map(([x, y]) => x + 8 * y);
     return legalMovePositions;
   }
 
@@ -54,8 +52,29 @@ export default class KnightMovesTree {
     }
     return root;
   }
+
+  #getPositionsPathToPos(endPos, startNode = this.#root) {
+    if (startNode.pos === endPos) return [startNode.pos];
+    if (startNode.children.length === 0) return null;
+    let path;
+    startNode.children.find((childNode) => {
+      path = this.#getPositionsPathToPos(endPos, childNode);
+      if (path != null) {
+        path.unshift(startNode.pos);
+        return true;
+      }
+      return false;
+    }, this);
+    return path;
+  }
+
+  getPathToPos([endX, endY]) {
+    const endPos = endX + 8 * endY;
+    const positionsPath = this.#getPositionsPathToPos(endPos);
+    const coordinatesPath = positionsPath.map((pos) => [
+      pos % 8,
+      Math.floor(pos / 8),
+    ]);
+    return coordinatesPath;
+  }
 }
-
-const kmt = new KnightMovesTree([0, 0]);
-
-console.log(kmt.getPathToPos(25));
